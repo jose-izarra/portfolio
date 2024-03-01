991f0432535eff35396cf54ac889574315377bd5
jose-izarra <jaiza0912@gmail.com>
2024-03-01 12:42:47 +0100

    started: about section + aceternity components

--- frontend/pages/aboutme.tsx
+++ frontend/pages/aboutme.tsx
@@ -1,9 +1,23 @@
 import Layout from '../components/layout';
 import UnderConstruction from '../components/underConstruction';
-
+import {
+    TextRevealCard,
+    TextRevealCardDescription,
+    TextRevealCardTitle,
+  } from "../components/ui/text-reveal-card";
 
 export default function AboutMe() {
     return (
-        <UnderConstruction />
+        <Layout page="About" title="About | Jose Izarra">
+            <article  className='min-h-[80vh] flex flex-col items-center justify-center'>
+                <h1 className=''>Hello, I am Jose!</h1>
+                {/* <h3>I love building and coding stuff in my free time</h3> */}
+                <TextRevealCard
+                    text="I love coding stuff in my free time"
+                    revealText="I get bored in my free time"
+                ></TextRevealCard>
+                
+            </article>
+        </Layout>
     )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -15,15 +15,15 @@ module.exports = {
         'title': "#FFE1DA",//"#E88349",//"#F39C6B", 
         // 'subtitle': "#FFE1DA",//"#EEEEFF",
         'subtitle': "#BB00CC",
-        'black': "#000",
-        'white': '#ffffff',
-        // 'cool': "#639FAB",
-        // 'cool-2' : "hsla(165, 100%, 40%, 1)",
+        
+        'white-color': 'hsla(162, 0%, 90%)',
+        'black-color': 'hsla(162, 50%, 10%, 1)',
+
         'cool' : "hsla(175, 100%, 40%, 1)",
         'ok' : "#cbd5e1",
         "lightbg-text" : "#5a5a66",
         'darkbg-text' : "#cbd5e1",
-        'nav-links': 'hsla(11, 100%, 93%, 1);'
+        'nav-links': 'hsla(175, 100%, 30%, 1)'
       },
     },
     
