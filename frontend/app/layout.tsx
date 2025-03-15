8ac6a6350e535a5dbc7d90664f4d3267f71bb1aa
jose-izarra <jaiza0912@gmail.com>
2025-03-15 20:50:29 +0100

    google analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 import { Analytics } from '@vercel/analytics/next';
-
+import Script from "next/script";
 export const metadata: Metadata = {
     title: "Jose Izarra",
     description: "Personal website",
@@ -31,6 +31,17 @@ export default async function RootLayout({
 
     return (
         <html lang="en" className="">
+            <head>
+                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-54D6PWBZ8R"></Script>
+                <Script id="google-analytics">
+                    {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-54D6PWBZ8R');
+                    `}
+                </Script>
+            </head>
             <body className={`${inter.className} `}>
                 {children}
                 <Footer />
